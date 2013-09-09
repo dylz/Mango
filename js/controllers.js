19f8f6407bbd0a6956ce114337ae8c6dcaf6f07d
Dylan <ddemoe1@gmail.com>
2013-09-09 00:50:27 -0400

    Changed flavmarks to flavourpages, and added routing linked to user and drop down

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,35 @@
 /*Controllers */
 
+/* Page status depending on app */
+function pageStatus($scope,$http,$location){
+	
+	$scope.appInfo = [];
+
+	$scope.loadAppInfo = function(data,status){
+		$scope.appInfo = data;
+	}
+
+	$scope.fetch = function(){
+		$http.get($scope.appJSON).success($scope.loadAppInfo);
+	}
+
+
+	$scope.$on('updatePage', function() {
+		$scope.loc = $location.path();
+		$scope.appJSON = 'app'+$scope.loc+'/manifest.json';
+		if(($scope.loc != '') && ($scope.loc != '/')){
+			$scope.loc = $scope.loc.substring(1);
+			$scope.appJSON = 'app/'+$scope.loc+'/manifest.json';
+		}
+		else{
+			$scope.appJSON = 'manifest.json';
+		}
+		$scope.fetch();
+	});
+}
+
 /*Load the Users info */
-function userInfo($scope,$http){
+function userInfo($scope,$http,$location){
 	$scope.userJSON = 'json/user.json';
 	$scope.userInfo = [];
 
@@ -13,15 +41,32 @@ function userInfo($scope,$http){
 		$http.get($scope.userJSON).success($scope.loadUserInfo);
 	}
 
+	$scope.currentAppCheck = function(app){
+		$scope.currentApp = $location.path().substring(1);
+		if($scope.currentApp != app){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
 	$scope.fetch();
 }
 
 /* Page Controllers For routing */
 function PagesController($scope, $http, $route, $routeParams, $compile) {
-  $route.current.templateUrl = 'app/' + $routeParams.name + '/index.html';
-  
-  $http.get($route.current.templateUrl).then(function (msg) {
-    $('#views').html($compile(msg.data)($scope));
-  });
+
+  if($routeParams.name !== undefined){
+  	$route.current.templateUrl = 'app/' + $routeParams.name + '/index.html';
+  	$http.get($route.current.templateUrl).then(function (msg) {
+    	$('#views').html($compile(msg.data)($scope));
+    	$scope.$emit('updatePage');
+  	});
+  }
+  else{
+  	$scope.$emit('updatePage');
+  }
 }
 PagesController.$inject = ['$scope', '$http', '$route', '$routeParams', '$compile'];
+
